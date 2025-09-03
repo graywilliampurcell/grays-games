f0e877ac6fd1b137a5556922f98239b4d21f83fd
Gray Purcell <graywilliampurcell@gmail.com>
2025-09-03 08:09:28 -0400

    Add publishing

---
+++ docs/books/the-book-that-can-read-your-mind/generate.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+# 1. Create a PDF
+PDF_FILE='dist/The Book That Can Read Your Mind: Gray Edition.pdf'
+if [ ! -f "$PDF_FILE" ]
+then
+    echo "Generating PDF..."
+    convert mod/*.png $PDF_FILE
+else
+    echo "PDF already exists."
+fi
+
+# 2. Create an EPUB
+EPUB_FILE='dist/The Book That Can Read Your Mind: Gray Edition.epub'
+if [ ! -f "$EPUB_FILE" ]
+then
+    echo "Generating EPUB..."
+    pandoc template/epub.html -o "$EPUB_FILE"
+else
+    echo "EPUB already exists."
+fi
